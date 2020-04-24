15ed9d4c48b68af951bbbd3f4e9d070dc8c9f763
Thiago Dorneles <thiagodornelesrs@gmail.com>
2020-04-23 21:03:25 -0300

    configurando Webpack para usar o DevServer

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,9 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    hot: true // monitorando as alterações e recompilando tudo sozinho
   }
 }
